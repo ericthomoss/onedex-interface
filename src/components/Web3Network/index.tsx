5935086b03b037c014c6896fc56bb54f281d6d79
Eric Thomas <bluebird6922@gmail.com>
2022-02-07 19:36:35 +0900

    auto switch network

--- src/components/Web3Network/index.tsx
+++ src/components/Web3Network/index.tsx
@@ -1,16 +1,43 @@
+import { ChainId } from '@sushiswap/core-sdk'
 import { NETWORK_ICON } from 'app/config/networks'
-import NetworkModel from 'app/modals/NetworkModal'
+import NetworkModel, { SUPPORTED_NETWORKS, switchNetwork } from 'app/modals/NetworkModal'
 import { useActiveWeb3React } from 'app/services/web3'
-import { useNetworkModalToggle } from 'app/state/application/hooks'
+//import { useNetworkModalToggle } from 'app/state/application/hooks'
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function Web3Network(): JSX.Element | null {
-  const { chainId } = useActiveWeb3React()
+  const { library, account, chainId } = useActiveWeb3React()
 
-  const toggleNetworkModal = useNetworkModalToggle()
+  //const toggleNetworkModal = useNetworkModalToggle()
 
   if (!chainId) return null
+
+  useEffect(() => {
+    const switchNetwork = async (key: ChainId) => {
+      const params = SUPPORTED_NETWORKS[key]
+
+      try {
+        await library?.send('wallet_switchEthereumChain', [{ chainId: `0x${key.toString(16)}` }, account])
+      } catch (switchError) {
+        // This error code indicates that the chain has not been added to MetaMask.
+        // @ts-ignore TYPE NEEDS FIXING
+        if (switchError.code === 4902) {
+          try {
+            await library?.send('wallet_addEthereumChain', [params, account])
+          } catch (addError) {
+            // handle "add" error
+            console.error(`Add chain error ${addError}`)
+          }
+        }
+        console.error(`Switch chain error ${switchError}`)
+        // handle other "switch" errors
+      }
+    }
+    if (chainId != ChainId.HARMONY && chainId != ChainId.HARMONY_TESTNET) {
+      switchNetwork(ChainId.HARMONY)
+    }
+  }, [library, account, chainId])
 
   return (
     <div
